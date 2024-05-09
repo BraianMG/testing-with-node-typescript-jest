94ab500a3817d0286db310f7414b5e28143b12e6
Braian Gonzales <braian_gonzales@ayscom.com>
2024-05-09 09:38:23 +0200

    jest project setup

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  testPathIgnorePatterns: ['/node_modules/', '/repo-original'],
+};
+
+export default config;
