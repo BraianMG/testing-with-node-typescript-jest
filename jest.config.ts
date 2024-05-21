4387457a39c43e7c4fabf40f5b2c338861abbd40
Braian Gonzales <braian_gonzales@ayscom.com>
2024-05-21 09:36:09 +0200

    pass checker setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,8 @@
 import { Config } from '@jest/types';
 
+const baseDir = '<rootDir>/src/app/pass_checker';
+const baseTestDir = '<rootDir>/src/test/pass_checker';
+
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
@@ -7,10 +10,11 @@ const config: Config.InitialOptions = {
   testPathIgnorePatterns: ['/node_modules/', '/repo-original'],
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/src/app/**/*.ts',
+    `${baseDir}/**/*.ts`,
     '!<rootDir>/node_modules/**',
     '!<rootDir>/repo-original/**',
   ],
+  testMatch: [`${baseTestDir}/**/*.ts`],
 };
 
 export default config;
